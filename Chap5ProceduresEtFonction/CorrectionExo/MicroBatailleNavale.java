65c579121ace68ef485274cc983c5c788de7d5bc
heroldg <80465743+heroldg@users.noreply.github.com>
2024-06-02 17:07:37 +0200

    Lol

---
+++ Chap5ProceduresEtFonction/CorrectionExo/MicroBatailleNavale.java
@@ -0,0 +1,82 @@
+package Chap5ProceduresEtFonction.CorrectionExo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MicroBatailleNavale {
+    // Taille du plateau de jeu
+    static final int TAILLE = 5;
+    static char[][] plateau = new char[TAILLE][TAILLE];
+    // Position du bateau
+    static int bateauX;
+    static int bateauY;
+
+    // Initialiser le plateau de jeu avec des '~' (eau)
+    public static void initialiserPlateau() {
+        for (int i = 0; i < TAILLE; i++) {
+            for (int j = 0; j < TAILLE; j++) {
+                plateau[i][j] = '~';
+            }
+        }
+    }
+
+    // Placer le bateau à une position aléatoire
+    public static void placerBateau() {
+        Random rand = new Random();
+        bateauX = rand.nextInt(TAILLE);
+        bateauY = rand.nextInt(TAILLE);
+    }
+
+    // Afficher le plateau de jeu
+    public static void afficherPlateau() {
+        for (int i = 0; i < TAILLE; i++) {
+            for (int j = 0; j < TAILLE; j++) {
+                System.out.print(plateau[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Demander à l'utilisateur de saisir un entier entre min et max (inclus)
+    public static int saisirEntierEntreBornes(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        int valeur;
+        do {
+            System.out.println("Veuillez saisir un entier entre " + min + " et " + max + ":");
+            valeur = scanner.nextInt();
+        } while (valeur < min || valeur > max);
+        return valeur;
+    }
+
+    // Permettre au joueur de choisir ses coordonnées de tir
+    public static boolean tirer() {
+        System.out.println("Coordonnées de tir:");
+        int x = saisirEntierEntreBornes(0, TAILLE - 1);
+        int y = saisirEntierEntreBornes(0, TAILLE - 1);
+
+        // Vérifier si le bateau est touché
+        if (x == bateauX && y == bateauY) {
+            plateau[x][y] = 'X';  // Marquer le coup réussi sur le plateau
+            System.out.println("Touché!");
+            return true;
+        } else {
+            plateau[x][y] = 'O';  // Marquer le coup raté sur le plateau
+            System.out.println("Manqué!");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        initialiserPlateau();
+        placerBateau();
+        afficherPlateau();
+
+        boolean touche = false;
+        while (!touche) {
+            touche = tirer();
+            afficherPlateau();
+        }
+        
+        System.out.println("Vous avez coulé le bateau!");
+    }
+}
--- Chap5ProceduresEtFonction/ExerciceProcedureEtFonction/MicroBaitailleNavaleV2.java
+++ Chap5ProceduresEtFonction/ExerciceProcedureEtFonction/MicroBaitailleNavaleV2.java
@@ -11,6 +11,9 @@ public class MicroBaitailleNavaleV2 {
         final int LARGEUR = 4;
         char[][] champsDeBaitaille = new char[LARGEUR][HAUTEUR];
 
+        int saisieColonne = 0;
+        int saisieLigne = 0;
+
         int aleatLargeur;
         aleatLargeur = (int)(random.nextInt(LARGEUR));
         int aleatHauteur;
@@ -20,7 +23,11 @@ public class MicroBaitailleNavaleV2 {
 
         afficherPlateau(champsDeBaitaille, aleatLargeur, aleatHauteur);
 
+        // saisirEntierEntreBornes(saisieColonne, saisieLigne, LARGEUR, HAUTEUR);
         
+        tirer(champsDeBaitaille, saisieColonne, saisieLigne, aleatLargeur, aleatHauteur, LARGEUR, HAUTEUR);
+            
+        console.close();
     }
 
     public static void initialiserPlateau(char[][] champsBataille, int l, int h, int lAleatoir, int hAleatoire) {
@@ -30,7 +37,7 @@ public class MicroBaitailleNavaleV2 {
                 
                 if (champsBataille[lAleatoir][hAleatoire] == champsBataille[i][j]) {
                     
-                    champsBataille[i][j] = 'x';
+                    champsBataille[i][j] = 'o';
                 }
             }
         }
@@ -48,12 +55,57 @@ public class MicroBaitailleNavaleV2 {
         }
     }
 
-    // public static saisirEntierEntreBornes() {
+    public static char[][] saisirEntierEntreBornes(char[][] tabValSaisie, int saisieColonne, int saisieLigne , int Largeur, int Hauteur) {
+        Scanner console = new Scanner(System.in); 
+
+       
+            while (saisieColonne >= Largeur) {
+                System.out.println("La colonne selectionner est hors du champs d'action, veuillez choisir entre 1 et " + Largeur + ":");
+                saisieColonne = console.nextInt() - 1;
+            }
+            
+            
+            while (saisieColonne >= Hauteur) {
+                System.out.println("La ligne selectionner est hors du champs d'action, veuillez choisir entre 1 et " + Hauteur + ":");
+            }
+            
         
+        
+        tabValSaisie[saisieColonne][saisieLigne] = 'x';
+        console.close();
 
-    // }
 
-    // public static tirer() {
+        return tabValSaisie ;
+        
+    }
 
-    // }
+    public static char tirer(char[][] tabChampsBataille  ,int saisieColonne, int saisieLigne, int cibleLargeur, int cibleHauteur, int Largeur, int Hauteur) {
+        Scanner console = new Scanner(System.in);
+        
+        do {
+            
+           
+            System.out.println("Quelle colonne ?");
+            saisieColonne = console.nextInt() - 1;
+            
+           
+
+            System.out.println("Quelle ligne ?");
+            saisieLigne = console.nextInt() - 1;
+            
+            tabChampsBataille[saisieLigne][saisieColonne] = 'x';
+            
+            // saisirEntierEntreBornes(tabChampsBataille, saisieColonne, saisieLigne, Largeur, Hauteur);
+
+            afficherPlateau(tabChampsBataille, cibleLargeur, cibleHauteur);
+
+            
+
+        } while (tabChampsBataille[saisieColonne][saisieLigne] != tabChampsBataille[cibleLargeur][cibleHauteur]);
+
+        System.out.println("cible touché !");
+
+        console.close();
+        return tabChampsBataille[saisieColonne][saisieLigne] = 'x';
+    }
 }
